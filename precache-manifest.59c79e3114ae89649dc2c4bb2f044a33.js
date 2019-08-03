b11b756488ea07979727e47b210401784bca72b1
cerib <impcesar@gmail.com>
2019-08-03 18:43:17 +0100

    Updates

---
+++ precache-manifest.59c79e3114ae89649dc2c4bb2f044a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f859984e1f1334160b6301a9e698b7f",
+    "url": "/tutorstartup/index.html"
+  },
+  {
+    "revision": "568af9b680fec7093906",
+    "url": "/tutorstartup/static/css/main.28e09470.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/tutorstartup/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "568af9b680fec7093906",
+    "url": "/tutorstartup/static/js/main.df267bd9.chunk.js"
+  },
+  {
+    "revision": "ce590852c38fade2a882",
+    "url": "/tutorstartup/static/js/runtime~main.1b5657f3.js"
+  }
+]);
